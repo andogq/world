b654e4c3cf73853a1db35417597cce045cfd1019
Tom Anderson <tom@ando.gq>
2020-06-03 12:10:20 +1000

    Add Channel class for promise based communication

---
+++ js/channel.js
@@ -0,0 +1,46 @@
+class Channel {
+    constructor(port, incomming) {
+        this.port = port;
+        this.incomming = incomming;
+
+        this.requests = [];
+
+        this.port.onmessage = (e) => {
+            let packet = e.data;
+
+            if (packet.type == "response") {
+                if (this.requests[packet.id]) {
+                    this.requests[packet.id](packet.data);
+                }
+            } else if (this.incomming) {
+                this.incomming(packet.type, packet.data).then((data) => {
+                    let response = {
+                        type: "response",
+                        data,
+                        id: packet.id
+                    };
+
+                    self.postMessage(response);
+                });
+            }
+        }
+    }
+
+    send(type, data, transferables) {
+        return new Promise((resolve) => {
+            let id = this.requests.length;
+
+            let packet = {
+                type,
+                data,
+                id
+            }
+
+            this.requests.push(resolve);
+
+            this.port.postMessage(packet, transferables);
+        });
+    }
+}
+
+export {Channel};
