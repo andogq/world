85ccf65aa1caae2529bbc49134dafef85886a597
Tom Anderson <tom@ando.gq>
2020-06-03 12:16:10 +1000

    Base Thread class

--- js/thread.js
+++ js/thread.js
@@ -1,36 +1,11 @@
-class Thread {
-    constructor(name, initData, transferables) {
-        this.messages = {};
+import {Channel} from "/js/channel.js";
 
-        this.worker = new Worker(`/js/workers/${name}.js`, {type: "module"});
-        this.worker.addEventListener("message", (e) => {
-            let promiseResolve = this.messages[e.data.id];
-            if (promiseResolve) {
-                promiseResolve(e.data);
-                this.messages[e.data.id] = undefined;
-            }
-        });
+class Thread extends Channel {
+    constructor(name, initData) {
+        super(new Worker(`/js/workers/${name}.js`, {type: "module"}));
 
-        this.send("init", initData, transferables);
-    }
-
-    send(type, data, transferables) {
-        return new Promise((resolve) => {
-            let id;
-
-            do {
-                id = Math.floor(Math.random() * Math.pow(10, 10)).toString(16);
-            } while (this.messages[id]);
-
-            this.messages[id] = resolve;
-    
-            this.worker.postMessage({
-                type,
-                data,
-                id
-            }, transferables);
-        });
+        this.send("init", initData).then(() => console.log("Init completed"));
     }
 }
 
-export {Thread};
+export {Thread}
